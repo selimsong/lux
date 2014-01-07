2abcf9da9bb54c919e160e5909f01a6cea85980a
selim song <selim@e.com>
2014-01-07 16:58:21 +0800

    'm'

--- data/template/default/head.php
+++ data/template/default/head.php
@@ -8,4 +8,12 @@
 <meta name="description" content="<?=$config['seo_description']?>" />
 <link rel="stylesheet" type="text/css" href="<?=$config['site_templateurl'];?>/css/common.css" />
 <script src="http://tjs.sjs.sinajs.cn/open/api/js/wb.js?appkey=" type="text/javascript" charset="utf-8"></script>
+<script type="text/javascript">
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	  ga('create', 'UA-46860957-1', 'yogurtdigital.com');
+	  ga('send', 'page view');
+</script>
 </head>
