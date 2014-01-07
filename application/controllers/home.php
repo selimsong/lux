95b5501e224588ab15dc853ce9d1f9779761da49
selim song <selim@e.com>
2014-01-07 13:19:27 +0800

    'm'

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -26,7 +26,15 @@ class Home extends CI_Controller {
 
         $tokenStringStr = json_decode($tokenStringAccess,true);
         if($tokenStringStr['oauth_token']){
+        	include_once( APPPATH.'third_party/sina/saetv2.ex.class.php' );
             set_cookie('oauth_token',$tokenStringStr['oauth_token'],3600*24);
+            $c = new SaeTClientV2( WB_AKEY , WB_SKEY , $tokenStringStr['oauth_token']);
+            $uid_get = $c->get_uid();
+            $_user = $this->Data_model->getSingle(array('user_id'=>$uid_get['uid']),'share_record');      
+            if (!empty($_user)) {
+            	redirect(base_url('index.php?/map'));
+            	exit();
+            }
             redirect(base_url('index.php?/welcome'));
         }
         $config = $this->Cache_model->loadConfig();
