d6fa3651653d57c71e05f84dc25e322c9ec74ece
selim song <selim@e.com>
2014-01-10 12:27:37 +0800

    'm'

--- application/third_party/sina/config.php
+++ application/third_party/sina/config.php
@@ -1,10 +1,11 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
-
 //define( "WB_AKEY" , '3371428860' );
 //define( "WB_SKEY" , '9878595d37d11ed9e1374d78959fb700' );
 //define( "WB_CALLBACK_URL" , 'http://weiboapp.wood-spring.com/index.php?/sina/callback?t=weibo' );
 
-define( "WB_AKEY" , '3648402289' );
-define( "WB_SKEY" , 'daaa900390b07f216a837ac90abe5033' );
+//define( "WB_AKEY" , '3648402289' );
+//define( "WB_SKEY" , 'daaa900390b07f216a837ac90abe5033' );
+define( "WB_AKEY" , '3371428860' );
+define( "WB_SKEY" , '9878595d37d11ed9e1374d78959fb700' );
 define( "WB_CALLBACK_URL" , 'http://lux.yogurtdigital.com/index.php?/sina/callback?t=weibo' );
